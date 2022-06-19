22a96e1ed4497d27c8216eed6251dda2bc6d2822
BoCyuanSu <j446214@gmail.com>
2022-06-19 22:58:00 +0800

    update productRouter

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -26,8 +26,7 @@ router.get('/', async (req, res, next) => {
   console.log('offset:', offset);
 
   // 5. 取得這一頁的資料
-  const sqlPage =
-    'SELECT product.id, product.product_name, product.price, product.image, vendor.business_name FROM product JOIN vendor ON product.vendor_id = vendor.id LIMIT ? OFFSET ?';
+  const sqlPage = 'SELECT product.id, product.product_name, product.price, product.image, vendor.business_name FROM product JOIN vendor ON product.vendor_id = vendor.id LIMIT ? OFFSET ?';
   let [pageProducts] = await pool.execute(sqlPage, [perPage, offset]);
 
   // 6. 回覆給前端
@@ -50,16 +49,14 @@ router.get('/classification', async (req, res, next) => {
 
 // localhost:3003/api/product/classification/1
 router.get('/classification/:classificationId', async (req, res, next) => {
-  const sql =
-    'SELECT classification.classification_name, category.category_name, product.id, product.category_id, product.product_name, product.price, product.image, vendor.business_name FROM product JOIN vendor ON product.vendor_id = vendor.id JOIN classification ON product.classification_id = classification.id JOIN category ON product.category_id = category.id WHERE product.classification_id = ?';
+  const sql = 'SELECT classification.classification_name, category.category_name, product.id, product.category_id, product.product_name, product.price, product.image, vendor.business_name FROM product JOIN vendor ON product.vendor_id = vendor.id JOIN classification ON product.classification_id = classification.id JOIN category ON product.category_id = category.id WHERE product.classification_id = ?';
   let [classification] = await pool.execute(sql, [req.params.classificationId]);
   res.json(classification);
 });
 
 // localhost:3003/api/product/category/1
 router.get('/category/:categoryId', async (req, res, next) => {
-  const sql =
-    'SELECT product.id, product.product_name, product.price, product.image, vendor.business_name FROM product JOIN vendor ON product.vendor_id = vendor.id WHERE product.category_id = ?';
+  const sql = 'SELECT product.id, product.product_name, product.price, product.image, vendor.business_name FROM product JOIN vendor ON product.vendor_id = vendor.id WHERE product.category_id = ?';
   let [category] = await pool.execute(sql, [req.params.categoryId]);
   res.json(category);
 });
