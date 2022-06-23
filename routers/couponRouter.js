924d363fa0a624d55c59e834d046de163115789f
cathysyu <siangjyun0926@gmail.com>
2022-06-23 21:57:34 +0800

    update router,server,gitignore

--- routers/couponRouter.js
+++ routers/couponRouter.js
@@ -2,9 +2,50 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../utils/database');
 
-// localhost:3003/api/product
+// localhost:3003/api/coupon
 router.get('/', async (req, res, next) => {
-    const sql = 'SELECT product.id, product.product_name, product.price, product.image, vendor.business_name FROM product JOIN vendor ON product.vendor_id = vendor.id'
-    let [products] = await pool.execute(sql);
-    res.json(products);
+  console.log('優惠券列表');
+  let [couponList] = await pool.execute('SELECT * FROM coupon');
+  res.json(couponList);
+});
+
+router.get('/:couponId', async (req, res, next) => {
+  // 取得網址上的參數 req.params
+  // req.params.couponId
+  console.log('get coupon by id', req.params);
+
+  let page = req.query.page || 1;
+  console.log('current page', page);
+
+  // 2. 取得目前的總筆數
+  let [allResults] = await pool.execute('SELECT * FROM coupon WHERE id = ?', [req.params.couponId]);
+  const total = allResults.length;
+  console.log('total:', total);
+
+  // 3. 計算總共有幾頁
+  // Math.ceil 1.1 => 2   1.05 -> 2
+  const perPage = 2; // 每一頁有幾筆
+  const lastPage = Math.ceil(total / perPage);
+  console.log('lastPage:', lastPage);
+
+  // 4. 計算 offset 是多少（計算要跳過幾筆）
+  // 在第五頁，就是要跳過 4 * perPage
+  let offset = (page - 1) * perPage;
+  console.log('offset:', offset);
+
+  // 5. 取得這一頁的資料 select * from table limit ? offet ?
+  let [pageCoupons] = await pool.execute('SELECT * FROM coupon WHERE id = ? ORDER BY couponList DESC LIMIT ? OFFSET ?', [req.params.couponId, perPage, offset]);
+
+  // 6. 回覆給前端
+  res.json({
+    // 用來儲存所有跟頁碼有關的資訊
+    pagination: {
+      total,
+      lastPage,
+      page,
+    },
+    // 真正的資料
+    couponList: pageCoupons,
   });
+});
+module.exports = router;
