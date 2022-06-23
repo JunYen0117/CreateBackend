ecc72b0362153165fda9a11e0b5bb28eefe84f8c
Miu37 <ispan8937@gmail.com>
2022-06-23 13:20:30 +0800

    Update productOrderRouter.js

--- routers/productOrderRouter.js
+++ routers/productOrderRouter.js
@@ -7,46 +7,54 @@ router.get('/', async (req, res, next) => {
   // 抓使用者id為1的訂單列表
   const sql2 = 'SELECT * FROM customer_order WHERE customer_order.customer_id = 1'; //5.6訂單
 
-  
   // 抓使用者id為1的訂單總數
   let [productorder] = await pool.execute(sql2);
-  let total = productorder.length;
-  console.log(total);
+  let orderId = [];
+  let orderDate =[]
+  productorder.map((v) => {
+    orderId.push(v.id);
+    orderDate.push(v.order_date);
+    return;
+  });
+  // console.log(orderId);
+  // 2筆訂單 （5,6)
 
-  
-  // id為1的分別買了哪些東西 幾樣商品 product_id
+  //訂單內容的detail
   let totaldata = [];
-  for (let i = 0; i < total; i++) {
-    let [data] = await pool.execute('SELECT * FROM customer_order_detail WHERE order_id = ?', [i + 1]);
-    // console.log(data);
-    totaldata = [...totaldata, { data }];
-    // [{訂單1號買了哪些商品} ,{訂單2號...}.....]
-  }
+  for (let i = 0; i < orderId.length; i++) {
 
-  
+    console.log(orderDate[i])
+    let [product] = await pool.execute('SELECT * FROM customer_order_detail WHERE customer_order_detail.order_id = ?', [orderId[i]]);
+    totaldata = [...totaldata, { product }];
+  }
+  // console.log(totaldata)
+  // console.log(product);
+
   // 抓總金額
   let totalarr = [];
   let mydata = {};
   // i -> 訂單總數
   for (let i = 0; i < totaldata.length; i++) {
-    // [{ [0], [1], [2] } --> 3個訂單 
-    
+    // [{ [0], [1], [2] } --> 3個訂單
+
+    // console.log(totaldata[i].product.length)
     // j -> 訂單內的商品總數 （第幾個商品）
-      let result = 0;
-    for (let j = 0; j < totaldata[i].data.length; j++) {
-      result = result + totaldata[i].data[j].subtotal;
-      
+    let result = 0;
+    for (let j = 0; j < totaldata[i].product.length; j++) {
+      // console.log(totaldata[i].product[j]);
+      result = result + totaldata[i].product[j].subtotal;
     }
-    console.log(result);
-        // mydata = { orderid: 1 , totalsub: 總金額 , totalcount : 總數量 } 
-        mydata = { orderid: totaldata[i].data[0].order_id, totalsub: result, totalcount: totaldata[i].data.length };
-        totalarr = [...totalarr, mydata]
+    // console.log(result);
+    //mydata = { orderid: 1 , totalsub: 總金額 }
+    mydata = { orderid: totaldata[i].product[0].order_id, totalsub: result, orderdate: orderDate[i]};
+    totalarr = [...totalarr, mydata];
   }
-  // 
+  console.log(totalarr)
   res.json({
-    totalarr: totalarr, 
+    totalarr: totalarr,
   });
-}); 
+});
+
 router.get('/:orderId', async (req, res, next) => {
   const sql2 = 'SELECT * FROM customer_order ';
 
@@ -60,11 +68,6 @@ router.get('/:orderId', async (req, res, next) => {
     },
     data: data,
   });
-});
-
-router.get('/productorderdetail', async (req, res, next) => {
-  const sql =
-    'SELECT customer_order_detail.id, customer_order_detail.order_id, customer_order_detail.product_id,customer_order_detail.vendor_id, customer_order_detail.price, customer_order_detail.amount, customer_order_detail.subtotal FROM customer_order_detail JOIN customer_order ON customer_order_detail.order_id = customer_order.id JOIN product ON customer_order_detail.product_id = product.id JOIN vendor ON customer_order_detail.vendor_id = vendor.id WHERE customer_order_detail.order_id = 1';
 });
 
 module.exports = router;
