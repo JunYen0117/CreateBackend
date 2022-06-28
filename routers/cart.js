198f6e25874c1bf9c72953b1359a69cc811ae15b
BoCyuanSu <j446214@gmail.com>
2022-06-28 21:21:07 +0800

    [ADD] cart

--- routers/cart.js
+++ routers/cart.js
@@ -5,9 +5,35 @@ const pool = require('../utils/database');
 // localhost:3003/api/cart/orderDetails
 router.post('/orderDetails', async (req, res, next) => {
   // shippingData
-  console.log(req.body);
+  const shippingData = req.body.shippingData;
+  // console.log('shippingData : ', shippingData);
 
-  res.json({message: 'OK'})
+  // 收件資料
+  const recipient = shippingData.recipient;
+  const recipientEmail = shippingData.email;
+  const tel = shippingData.tel;
+  const address = shippingData.county + shippingData.district + shippingData.address;
+
+  // 購買商品清單
+  const checkList = req.body.checkList;
+  const total = req.body.total;
+
+  // 目前時間
+  const date = new Date();  
+
+  // customer_order
+  const sql = 'INSERT INTO customer_order (customer_id, status, recipient, recipient_email, tel, address, order_date, total, valid) VALUES (1, 1, ?, ?, ?, ?, ?, ?, 1)';
+  const [order] = await pool.execute(sql, [recipient, recipientEmail, tel, address, date, total]);
+
+  // customer_order_detail
+  const orderId = order.insertId;
+  let sqlDetail = 'INSERT INTO customer_order_detail (order_id, product_id, vendor_id, price, amount, subtotal) VALUES (?, ?, ?, ?, ?, ?)';
+  for (let i = 0; i < checkList.length; i++) {
+    let subtotal = checkList[i].price * checkList[i].quantity;
+    let [orderProduct] = await pool.execute(sqlDetail, [orderId, checkList[i].id, checkList[i].vendor_id, checkList[i].price, checkList[i].quantity, subtotal]);
+  }
+
+  res.json({message: '歡迎下次再次光臨'})
 })
 
 
